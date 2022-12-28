b1179a7a727b8a4fb65aeda65ff67b428dcc51f8
szhengjiaanv <120627122+szhengjiaanv@users.noreply.github.com>
2022-12-28 17:15:59 +0800

    更新 pxt.json, main.ts, _locales/zh-CN/Lbit-strings.json

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
+//% color="#610001" weight=10 
+namespace Lbit {
 
+    //% blockId="opentest" block="opentest"
+    //% weight=55
+    export function opentest(): void {
+        let i = 0
+    }
+
+    //% blockId="test2" block="test2"
+    //% weight=50
+    export function test2(): void {
+        let i = 0
+    }
+}
